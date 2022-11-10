0b947496770ae0df8a99c883721309e539fcc251
kazi naeem rayhan <kazinaeemrayhan@gmail.com>
2022-11-10 21:14:45 +0600

    first commit

---
+++ components/About.jsx
@@ -0,0 +1,60 @@
+import React from "react";
+import Image from "next/image";
+import Link from "next/link";
+import AboutImg from "../public/assets/about.jpg";
+
+const About = () => {
+  return (
+    <div id="about" className="w-full md:h-screen p-2 flex items-center py-16">
+      <div className="max-w-[1240px] m-auto md:grid grid-cols-3 gap-8">
+        <div className="col-span-2">
+          <p className="uppercase text-xl tracking-widest text-[#5651e5]">
+            About
+          </p>
+          <h2 className="py-4">Who I Am</h2>
+          <ul className="">
+            <li className="d-flex">
+              <span>Name:</span> <span>Kazi Naeem Rayhan</span>
+            </li>
+            <li className="d-flex">
+              <span>Address:</span> <span>Dhaka, Banglasesh</span>
+            </li>
+            <li className="d-flex">
+              <span>Email:</span> <span>Kazinaeemrayhan@gmail.com</span>
+            </li>
+            <li className="d-flex">
+              <span>Phone: </span> <span>+880 1751 449559 </span>
+            </li>
+          </ul>
+          <p className="py-2 mt-4 text-gray-600">
+            I am very dedicated and passionate about programming. I am an
+            adaptable person who is looking forward to learning new techniques,
+            skills, and methods in order to gain experience that can be used in
+            a career in the near and distant future.
+          </p>
+
+          <p className="my-3">
+            <a
+              href="file//Resume of kazi naeem rayhan.pdf"
+              className="py-3 px-3 bg-violet-600 text-white rounded-2xl"
+              download
+            >
+              Download Resume
+            </a>
+          </p>
+
+          <Link href="/#projects">
+            <p className="py-2 text-gray-600 underline cursor-pointer">
+              Check out some of my latest projects.
+            </p>
+          </Link>
+        </div>
+        <div className="w-full h-auto m-auto shadow-xl shadow-gray-400 rounded-xl flex items-center justify-center p-4 hover:scale-105 ease-in duration-300">
+          <Image src={AboutImg} className="rounded-xl" alt="/" />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default About;
---
+++ components/Main.jsx
@@ -0,0 +1,61 @@
+import Link from "next/link";
+import React from "react";
+import { AiOutlineMail } from "react-icons/ai";
+import { BsFillPersonLinesFill } from "react-icons/bs";
+import { FaGithub, FaLinkedinIn } from "react-icons/fa";
+
+const Main = () => {
+  return (
+    <div id="home" className="w-full h-screen text-center">
+      <div className="max-w-[1240px] w-full h-full mx-auto p-2 flex justify-center items-center">
+        <div>
+          <p className="uppercase text-sm tracking-widest text-gray-600">
+            LET&#39;S BUILD SOMETHING TOGETHER
+          </p>
+          <h1 className="py-4 text-gray-700">
+            Hi, I&#39;m{" "}
+            <span className="text-[#5651e5]">Kazi Naeem Rayhan</span>
+          </h1>
+          <h1 className="py-2 text-gray-700">A MERN Stack Developer</h1>
+          <p className="py-4 text-gray-600 sm:max-w-[70%] m-auto">
+            I have intermediate skill in Node, Express, MongoDB, ReactJS,
+            Docker, Redux, Mongoose, Typescript, GraphQL, CI/CD Pipeline, PM2,
+            Git, JavaScript, HTML5, CSS3.
+          </p>
+          <div className="flex items-center justify-between max-w-[330px] m-auto py-4">
+            <a
+              href="https://www.linkedin.com/in/kazinaeemrayhan"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <div className="rounded-full shadow-lg shadow-gray-400 p-6 cursor-pointer hover:scale-110 ease-in duration-300">
+                <FaLinkedinIn />
+              </div>
+            </a>
+            <a
+              href="https://github.com/knrbokhari"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <div className="rounded-full shadow-lg shadow-gray-400 p-6 cursor-pointer hover:scale-110 ease-in duration-300">
+                <FaGithub />
+              </div>
+            </a>
+            <Link href="/#contact">
+              <div className="rounded-full shadow-lg shadow-gray-400 p-6 cursor-pointer hover:scale-110 ease-in duration-300">
+                <AiOutlineMail />
+              </div>
+            </Link>
+            <Link href="/resume">
+              <div className="rounded-full shadow-lg shadow-gray-400 p-6 cursor-pointer hover:scale-110 ease-in duration-300">
+                <BsFillPersonLinesFill />
+              </div>
+            </Link>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Main;
