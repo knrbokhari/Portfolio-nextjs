c103e5a438a5f2b0963ff40a3b7e16fdbc431c98
kazi naeem rayhan <kazinaeemrayhan@gmail.com>
2022-11-11 23:21:17 +0600

    project added

--- components/Projects.jsx
+++ components/Projects.jsx
@@ -1,47 +1,44 @@
-import Image from 'next/image';
-import Link from 'next/link';
-import React from 'react';
-import propertyImg from '../public/assets/projects/property.jpg';
-import cryptoImg from '../public/assets/projects/crypto.jpg'
-import netflixImg from '../public/assets/projects/netflix.jpg'
-import twitchImg from '../public/assets/projects/twitch.jpg'
-import ProjectItem from './ProjectItem';
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
+import social from "../public/assets/projects/social.jpg";
+import superSystem from "../public/assets/projects/super-system.jpg";
+import ecom from "../public/assets/projects/ecom.png";
+import manufacturer from "../public/assets/projects/manufacturer.jpg";
+import ProjectItem from "./ProjectItem";
 
 const Projects = () => {
   return (
-    <div id='projects' className='w-full'>
-      <div className='max-w-[1240px] mx-auto px-2 py-16'>
-        <p className='text-xl tracking-widest uppercase text-[#5651e5]'>
+    <div id="projects" className="w-full">
+      <div className="max-w-[1240px] mx-auto px-2 py-16">
+        <p className="text-xl tracking-widest uppercase text-[#5651e5]">
           Projects
         </p>
-        <h2 className='py-4'>What I&apos;ve Built</h2>
-        <div className='grid md:grid-cols-2 gap-8'>
+        <h2 className="py-4">What I&apos;ve Built</h2>
+        <div className="grid md:grid-cols-2 gap-8">
           <ProjectItem
-            title='Property Finder'
-            backgroundImg={propertyImg}
-            projectUrl='/property'
-            tech='React JS'
+            title="Ecommerce Website"
+            backgroundImg={ecom}
+            projectUrl="/netflix"
+            tech="MERN Stack Project"
           />
           <ProjectItem
-            title='Crypto App'
-            backgroundImg={cryptoImg}
-            projectUrl='/crypto'
-            tech='React JS'
-
+            title="Social Media App"
+            backgroundImg={social}
+            projectUrl="/property"
+            tech="MERN Stack Project"
           />
           <ProjectItem
-            title='Netflix App'
-            backgroundImg={netflixImg}
-            projectUrl='/netflix'
-            tech='React JS'
-
+            title="Inventory Management System"
+            backgroundImg={superSystem}
+            projectUrl="/crypto"
+            tech="MERN Stack Project"
           />
           <ProjectItem
-            title='Twitch UI'
-            backgroundImg={twitchImg}
-            projectUrl='/twitch'
-            tech='Next JS'
-
+            title="Manufacturer Website"
+            backgroundImg={manufacturer}
+            projectUrl="/twitch"
+            tech="MERN Stack Project"
           />
         </div>
       </div>
