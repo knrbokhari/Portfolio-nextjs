5e572c89eacd0d05aab7dca19bb980f77c9715b0
kazi naeem rayhan <kazinaeemrayhan@gmail.com>
2022-12-01 23:10:50 +0600

    ecommerce page added

---
+++ pages/ecommerce.jsx
@@ -0,0 +1,87 @@
+import Image from 'next/image';
+import React from 'react';
+import ecommerce from '../public/assets/projects/ecom.png';
+import { RiRadioButtonFill } from 'react-icons/ri';
+import Link from 'next/link';
+
+const netflix = () => {
+  return (
+    <div className='w-full'>
+      <div className='w-screen h-[50vh] relative'>
+        <div className='absolute top-0 left-0 w-full h-[50vh] bg-black/70 z-10' />
+        <Image
+          className='absolute z-1'
+          layout='fill'
+          objectFit='cover'
+          src={ecommerce}
+          alt='/'
+        />
+        <div className='absolute top-[70%] max-w-[1240px] w-full left-[50%] right-[50%] translate-x-[-50%] translate-y-[-50%] text-white z-10 p-2'>
+          <h2 className='py-2'>Ecommerce App</h2>
+          <h3>React JS / Node JS / Express / MomgoDB</h3>
+        </div>
+      </div>
+
+      <div className='max-w-[1240px] mx-auto p-2 grid md:grid-cols-5 gap-8 py-8'>
+        <div className='col-span-4'>
+          <p>Project</p>
+          <h2>Overview</h2>
+          <p>
+            <ul>
+                <li>
+                    This website has jwt implemented, email, password, authentication, and a stripe payment system. It has an error log system that stores in MongoDB.
+                </li>
+                <li>
+                    Users can add items to the cart, change cart quantity, and remove items from the card. it gives users a notification when the order is shipped.
+                </li>
+                <li>
+                    Admin can update product status and restock, add, and delete products. When users order products then the admin gets a notification from it.
+                </li>
+            </ul>
+          </p>
+          <a
+            href='https://github.com/fireclint/netflix-react-tailwind'
+            target='_blank'
+            rel='noreferrer'
+          >
+            <button className='px-8 py-2 mt-4 mr-8'>Code</button>
+          </a>
+          <a
+            href='https://fireclint.github.io/netflix-react-tailwind/'
+            target='_blank'
+            rel='noreferrer'
+          >
+            <button className='px-8 py-2 mt-4'>Live</button>
+          </a>
+        </div>
+        <div className='col-span-4 md:col-span-1 shadow-xl shadow-gray-400 rounded-xl py-4'>
+          <div className='p-2'>
+            <p className='text-center font-bold pb-2'>Technologies</p>
+            <div className='grid grid-cols-3 md:grid-cols-1'>
+              <p className='text-gray-600 py-2 flex items-center'>
+                <RiRadioButtonFill className='pr-1' /> React
+              </p>
+              <p className='text-gray-600 py-2 flex items-center'>
+                <RiRadioButtonFill className='pr-1' /> Tailwind
+              </p>
+              <p className='text-gray-600 py-2 flex items-center'>
+                <RiRadioButtonFill className='pr-1' /> Javascript
+              </p>
+              <p className='text-gray-600 py-2 flex items-center'>
+                <RiRadioButtonFill className='pr-1' /> Firebase
+              </p>
+              <p className='text-gray-600 py-2 flex items-center'>
+                <RiRadioButtonFill className='pr-1' /> IMDB API
+              </p>
+            </div>
+          </div>
+        </div>
+        <Link href='/#projects'>
+          <p className='underline cursor-pointer'>Back</p>
+        </Link>
+      </div>
+    </div>
+  );
+};
+
+export default netflix;
