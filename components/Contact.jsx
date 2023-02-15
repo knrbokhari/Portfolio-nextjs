7e1b18a2f1457c6f24a281c48768a8557e3a9c59
kazi naeem rayhan <kazinaeemrayhan@gmail.com>
2023-02-15 19:49:09 +0600

    gymWeb page added

--- components/Contact.jsx
+++ components/Contact.jsx
@@ -60,7 +60,7 @@ const Contact = () => {
                       <AiOutlineMail />
                     </div>
                   </a>
-                  <Link href="/resume">
+                  <Link href="/">
                     <a>
                       <div className="rounded-full shadow-lg shadow-gray-400 p-6 cursor-pointer hover:scale-110 ease-in duration-300">
                         <BsFillPersonLinesFill />
